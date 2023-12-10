ca43a695a0e18f41dd866ebddf3799bc69b3fa03
rasik-hasan <rasik.hasan14@gmail.com>
2023-12-10 16:23:31 +0900

    exponential back off set

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,16 +3,30 @@ import { ISiteOutage } from "../types/types";
 
 // 1. `GET /outages` which returns all outages in our system
 export async function getOutages(API_URL: string, header: {}) {
-  try {
-    const res = await axios.get(API_URL + "/outages", {
-      headers: header,
-    });
-    // console.log("Res:", res.data);
+  const tryMax = 3;
+  const baseDelay = 1000;
+  let tryCounter = 0;
 
-    return res.data;
-  } catch (error) {
-    console.log("Error: ", error);
+  while (tryCounter < tryMax) {
+    try {
+      const res = await axios.get(API_URL + "/outages", {
+        headers: header,
+      });
+      // console.log("Res:", res.data);
+
+      return res.data;
+    } catch (error) {
+      console.log("Error: ", error, "CurrentTry:", tryCounter);
+
+      const delay = Math.pow(2, tryCounter) * baseDelay;
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      tryCounter++;
+    }
   }
+
+  throw new Error("Exponential Back Off failed in GetOutages");
 }
 
 // 2. `GET /site-info/{siteId}` which returns specific information about a site
@@ -21,15 +35,29 @@ export async function getSiteInfo(
   header: {},
   siteId: string
 ): Promise<any> {
-  try {
-    const res = await axios.get(API_URL + `/site-info/${siteId}`, {
-      headers: header,
-    });
+  const tryMax = 3;
+  const baseDelay = 1000;
+  let tryCounter = 0;
 
-    return res.data;
-  } catch (error) {
-    console.log("Error:", error);
+  while (tryCounter < tryMax) {
+    try {
+      const res = await axios.get(API_URL + `/site-info/${siteId}`, {
+        headers: header,
+      });
+
+      return res.data;
+    } catch (error) {
+      console.log("Error: ", error, "CurrentTry:", tryCounter);
+
+      const delay = Math.pow(2, tryCounter) * baseDelay;
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      tryCounter++;
+    }
   }
+
+  throw new Error("Exponential Back Off failed in GetSiteOutages");
 }
 
 // 3. `POST /site-outages/{siteId}` which expects outages for a specific site to be posted to it
@@ -39,17 +67,31 @@ export async function postSiteOutages(
   siteOutages: ISiteOutage[],
   siteId: string
 ) {
-  try {
-    const res = await axios.post(
-      API_URL + `/site-outages/${siteId}`,
-      siteOutages,
-      { headers: header }
-    );
-    console.log("post status: ", res.status);
-    console.log("post res: ", res.data);
+  const tryMax = 3;
+  const baseDelay = 1000;
+  let tryCounter = 0;
 
-    return res;
-  } catch (error) {
-    console.log("Error:", error);
+  while (tryCounter < tryMax) {
+    try {
+      const res = await axios.post(
+        API_URL + `/site-outages/${siteId}`,
+        siteOutages,
+        { headers: header }
+      );
+      console.log("post status: ", res.status);
+      console.log("post res: ", res.data);
+
+      return res;
+    } catch (error) {
+      console.log("Error: ", error, "CurrentTry:", tryCounter);
+
+      const delay = Math.pow(2, tryCounter) * baseDelay;
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      tryCounter++;
+    }
   }
+
+  throw new Error("Exponential Back Off failed in PostSiteOutages");
 }
